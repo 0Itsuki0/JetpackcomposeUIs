26778073661bb6cd73869db035c32444e5b3fd8e
Itsuki <2018.itsuki@gmail.com>
2024-03-02 10:22:50 +0900

    Update PullToRefreshLoadMore.kt

--- PullToRefreshLoadMore.kt
+++ PullToRefreshLoadMore.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import jp.co.collabostyle.collaboflow.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
